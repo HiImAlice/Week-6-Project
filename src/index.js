199e6670d64b82b3cc654eff401a87252a301e97
HiImAlice <97409200+HiImAlice@users.noreply.github.com>
2022-01-16 15:24:39 -0800

    unit conversion fixed, added humidity and wind

--- src/index.js
+++ src/index.js
@@ -23,10 +23,26 @@ function handleSubmit(event) {
   searchCity(city);
 }
 function displayWeather(response) {
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let descriptionElement = document.querySelector("#description");
+  let iconElement = document.querySelector("#tempIcon");
   document.querySelector("#cityHeader").innerHTML = response.data.name;
   document.querySelector(".currentTemp").innerHTML = Math.round(
     response.data.main.temp
   );
+  fahrenheitTemp = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  windElement.innerHTML = `Wind Speed: ${Math.round(
+    response.data.wind.speed
+  )}mph`;
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+  searchLocation(response.data.coord);
 }
 function searchCity(city) {
   let apiKey = "49ca946f460f00fafc1fb63ecfc6fcf4";
@@ -40,13 +56,16 @@ let Flink = document.querySelector("#farenheit");
 let cLink = document.querySelector("#celsius");
 function tempToF(event) {
   event.preventDefault();
-  currentTemp.innerHTML = `66ºF`;
+  currentTemp.innerHTML = `${fahrenheitTemp}ºF`;
 }
 Flink.addEventListener("click", tempToF);
+let celsiusTemp = null;
+let fahrenheitTemp = null;
 
 function tempToC(event) {
   event.preventDefault();
-  currentTemp.innerHTML = `17ºC`;
+  celsiusTemp = Math.round(((fahrenheitTemp - 32) * 5) / 9);
+  currentTemp.innerHTML = `${celsiusTemp}ºC`;
 }
 cLink.addEventListener("click", tempToC);
 let cityForm = document.querySelector("#cityForm");
